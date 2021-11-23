4835c8576c94285173f9bd58fa41eae19178ad6b
raydegroat <stockholmray@gmail.com>
2021-11-23 14:06:19 +0100

    Add restore to main script

--- T5Backup.sh
+++ T5Backup.sh
@@ -40,7 +40,34 @@ then
 	date
 	break
 else
-	echo "Enter file to restore"
+	read -p "Enter the backup file to restore: " restore_files 
+	read -p "Enter the path to restore to: " dest
+	
+	# Print start status message.
+	echo "Restoring $restore_files to $dest"
+	date
+	echo
+	# To see the listing of archive contents
+	arc_cont=$(tar -tf $restore_files)
+	echo "contents of the archive files are: $arc_cont"
+	echo
+	# Restore the files using tar.
+	# Future error hadling and robustness
+	#if [ -f "$File" ]; then 
+	#	echo "File exists. No need to restore. Abort restoring"
+	#	echo
+	#else
+   	
+	# Extract backup files
+	tar -xzvf $restore_files -C $dest
+   	
+	# -C  option to tar redirects the extracted files to the specified directory.
+	#fi
+
+	# Print end status message.
+	echo
+	echo "Restore finished"
+	date
 	break
 fi
 done
