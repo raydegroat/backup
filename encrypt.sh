9c6b76da7991166ee4aa77652f728fa3dd89963b
raydegroat <stockholmray@gmail.com>
2021-11-23 13:09:34 +0100

    Added break statements to terminate select cases

--- encrypt.sh
+++ encrypt.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "A simple file encrypter/decrypter"
+# echo "A simple file encrypter/decrypter"
 
-echo "Select what you want to do"
+echo "Select 1 for Encrypt, 2 for decrypt"
 
 choice="Encryption Decryption"
 
@@ -12,12 +12,14 @@ then
 	echo "Enter the file name to encrypt"
 	read file;
 	gpg -c $file
-	echo "The file is encrypted"
+	echo "The file is encrypted" # Script should first verify sucess  before echoing  
+	break
 else
 	echo "Enter the file name to decrypt"
 	read file2;
 	gpg $file2
 	echo "The is decrypted" 
+	break
 fi
 
 done
