77443dd7f8e26f6dcd5203961e1dd67591fb1e20
raydegroat <stockholmray@gmail.com>
2021-11-23 19:33:40 +0100

    Cleaned up and commented. Removed unused scripts

--- T5Backup.sh
+++ T5Backup.sh
@@ -5,72 +5,78 @@
 #
 ####################################
 
-echo "Enter 1 to backup or 2 to restore"
+echo "Enter 1 to backup or 2 to restore" # Prompt user to select backup or restore
 
 choice="Backup Restore"
 
 select option in $choice; do
-	if [ $REPLY = 1 ];
+	if [ $REPLY = 1 ];		 # Backup
 then
-	# What to backup. --Anwar added
-	read -p "Enter the files or folders with path to backup: " backup_files
-	# Where to backup to. -- Anwar added
-	read -p "Enter the destination path where to backup to: " dest
+					 # Determin source
+	read -p "Enter path of file or directory to backup: " source_file
+					 # Determin destination
+	read -p "Enter path to backup to: " dest_path
 
-	# Create archive filename.
-	day=$(date +%A)
+					 # Create an archive name using hostname, 
+	day=$(date +%A)			 # day of week, and ending with .tgz
 	hostname=$(hostname -s)
-	archive_file="$hostname-$day.tgz"
+	archive_name="$hostname-$day.tgz" 
 
-	# Print start status message.
-	echo "Backing up $backup_files to $dest/$archive_file"
+	echo "Archiving and compressing  $source_file to $dest_path/$archive_name"
 	date
 	echo
+				         # Create a gzip tar archive 
+	tar czf $dest_path/$archive_name $source_file
 
-	# Backup the files using tar.
-	tar czf $dest/$archive_file $backup_files
-
-	gpg -c $dest/$archive_file
+	echo "Encrypting $archive_name"
+	echo	 
+					 # Encrypt the file using GPG symetric encryption
+	gpg -c $dest_path/$archive_name
 	
-
 	echo "The file is encrypted. Deleting unencrypted archive..."
-	rm -f $dest/$archive_file
-	# Script should first verify sucess  before echoing sucess
-	# Print end status message.
 	echo
-	echo "Backup and encrypt finished!"
+	rm -f $dest_path/$archive_name
+	echo "Backup and encrypt complete!"
 	date
 	break
-else
-	read -p "Enter the encrypted backup file to restore: " restore_files 
-	read -p "Enter the path to restore to: " dest
-	# Print start status message.
-	echo "Restoring $restore_files to $dest"
+else					 # Restore - Determin source and destination
+	read -p "Enter path to encrypted backup file to restore from: " restore_file 
+	read -p "Enter the path to restore to: " dest_path
+
+	echo "Decrypting $restore_file to $dest_path"
 	date
 	echo
-	gpg -d --output $dest/output.tgz $restore_files
+					 # Decrypt file
+	gpg -d --output $dest_path/temp_decrypted.tgz $restore_file
+	
+	echo "File decrypted"
+	echo
+	
+	# Future error handling for robustness
 	# To see the listing of archive contents
 	# arc_cont=$(tar -tf $restore_files)
 	# echo "contents of the archive files are: $arc_cont"
 	# Restore the files using tar.
-	# Future error hadling and robustness
 	#if [ -f "$File" ]; then 
 	#	echo "File exists. No need to restore. Abort restoring"
 	#	echo
 	#else
-   	
-	# Extract backup files
-	tar -xzvf $dest/output.tgz -C $dest
-   	
-	# -C  option to tar redirects the extracted files to the specified directory.
 	#fi
 
-	# Print end status message.
+	echo "Unarchiving..."
+	echo  	
+					 # Extract backup files using -C to direct 
+					 # files to specified directory.
+	tar -xzvf $dest_path/temp_decrypted.tgz -C $dest_path
+   	
+	echo "Unarchive complete!"
 	echo
-	echo "Restore finished"
+	echo "Cleaning up..."
+	echo 
+	echo "Restore finished."
 	date
 	break
 fi
 done
 # Long listing of files in $dest to check file sizes.
-ls -lh $dest
+ls -lh $dest_path
